7363a878c7dead6176fb5f01c4104002a4e820b7
Wouter J <wouterjansen4@gmail.com>
2017-03-27 21:09:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,6 +51,6 @@ EOF
 systemctl enable pulseaudio.service
 cd ..
 cd ./AlexaPi/src/
-sed -i '13s/.*/input_device: "pulse"/' config.yaml
-sed -i '22s/.*/output: "pulse"/' config.yaml
-sed -i '25s/.*/output_device: ""/' config.yaml
+sed -i '13s/.*/    input_device: "pulse"/' config.yaml
+sed -i '22s/.*/    output: "pulse"/' config.yaml
+sed -i '25s/.*/    output_device: ""/' config.yaml
