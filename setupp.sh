7e781e90d921f9e54439bdb59f7e65f2ff36de7f
Wouter J <wouterjansen4@gmail.com>
2017-03-27 21:37:04 +0200

    Update setupp.sh

--- setupp.sh
+++ setupp.sh
@@ -2,7 +2,7 @@
 
 set -o nounset # Fail when variable is used, but not initialized
 set -o errexit # Fail on unhandled error exits
-
+popd > /dev/null
 OS_default="pi"
 echo "Which username are you using?"
 read -r -p "Your username [${OS_default}]: " OS
