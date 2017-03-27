d000651271ad94c4351854a256a3bd23e7117f23
Wouter J <wouterjansen4@gmail.com>
2017-03-27 19:57:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "Installing all necessary tools for SHOOB AI"
-apt-get install build-essential python-dev git scons swig
+apt-get update
+apt-get install build-essential python-dev git scons swig -y
 cd ./rpi_ws281x
 scons
 cd ./python
 python setup.py install
 cd ..
 cd..
-sudo apt install pulseaudio pavucontrol
-sudo apt remove pavumeter paman padevchooser
+sudo apt install pulseaudio pavucontrol -y
+sudo apt remove pavumeter paman padevchooser -y
 cd python-pulseaudio
 python setup.py install
