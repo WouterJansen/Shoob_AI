5e6b9217aeb014f948c001c0db3e03c86a65f8dd
Wouter J <wouterjansen4@gmail.com>
2017-03-27 20:17:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Installing all necessary tools for SHOOB AI"
+echo ""
 apt-get update
+echo "-- Installing SCONS --"
+echo ""
 apt-get install build-essential python-dev git scons swig -y
 cd ./rpi_ws281x
 scons
@@ -8,7 +11,13 @@ cd ./python
 python setup.py install
 cd ..
 cd ..
+echo "-- Installing PulseAudio --"
+echo ""
 sudo apt install pulseaudio pavucontrol -y
 sudo apt remove pavumeter paman padevchooser -y
 cd python-pulseaudio
 python setup.py install
+echo "-- Disabling audio kernel module --"
+echo ""
+cat <<EOF >/etc/modprobe.d/snd-blacklist.conf
+blacklist snd_bcm2835
