bf2c98b55a7effc5547e7d599be7c6e5a1bbbd93
Wouter J <wouterjansen4@gmail.com>
2017-03-27 21:35:47 +0200

    Update setupp.sh

--- setupp.sh
+++ setupp.sh
@@ -1,7 +1,16 @@
-NAME_default="pi"
-echo "Which is the username?"
-read -r -p "Your username [${NAME_default}]: " NAME
-if [ "${NAME}" == "" ]; then
-    NAME=${NAME_default}
+#!/bin/bash
+
+set -o nounset # Fail when variable is used, but not initialized
+set -o errexit # Fail on unhandled error exits
+set -o pipefail # Fail when part of piped execution fails
+
+OS_default="pi"
+echo "Which username are you using?"
+read -r -p "Your username [${OS_default}]: " OS
+if [ "${OS}" == "" ]; then
+    OS=${OS_default}
+elif [ ! -f "./inc/os/${OS}.sh" ]; then
+    echo "Incorrect value. Exiting."
+    exit
 fi
-adduser "${NAME}" pulse-access
+adduser "${OS}" pulse-access
