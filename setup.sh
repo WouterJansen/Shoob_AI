4f8250c80aa4597030eccbcbe4d21a09a03934ea
Wouter J <wouterjansen4@gmail.com>
2017-03-27 20:20:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Installing all necessary tools for SHOOB AI"
+echo "Installing SHOOB AI dependencies"
 echo ""
 apt-get update
+echo ""
 echo "-- Installing SCONS --"
 echo ""
 apt-get install build-essential python-dev git scons swig -y
@@ -11,13 +12,16 @@ cd ./python
 python setup.py install
 cd ..
 cd ..
+echo ""
 echo "-- Installing PulseAudio --"
 echo ""
 sudo apt install pulseaudio pavucontrol -y
 sudo apt remove pavumeter paman padevchooser -y
 cd python-pulseaudio
 python setup.py install
+echo ""
 echo "-- Disabling audio kernel module --"
 echo ""
 cat <<EOF >/etc/modprobe.d/snd-blacklist.conf
 blacklist snd_bcm2835
+EOF
