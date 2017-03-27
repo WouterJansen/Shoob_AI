49ad96ec9a56c02101ba4bc6c94c2bae37dfb522
Wouter J <wouterjansen4@gmail.com>
2017-03-27 21:01:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,3 +25,27 @@ echo ""
 cat <<EOF >/etc/modprobe.d/snd-blacklist.conf
 blacklist snd_bcm2835
 EOF
+echo ""
+echo "-- Running PA in system-wide mode --"
+echo ""
+mkdir -p /var/lib/AlexaPi/.config/pulse
+cp /etc/pulse/client.conf /var/lib/AlexaPi/.config/pulse/
+echo "autospawn=no" | sudo tee --append /var/lib/AlexaPi/.config/pulse/client.conf > /dev/null
+chown -R alexapi:alexapi /var/lib/AlexaPi/
+usermod --home /var/lib/AlexaPi alexapi
+adduser pulse audio
+adduser <username or pi> pulse-access
+adduser alexapi pulse-access
+cat <<EOF >/etc/systemd/system/pulseaudio.service
+[Unit]
+Description=PulseAudio Daemon
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+Type=simple
+PrivateTmp=true
+ExecStart=/usr/bin/pulseaudio --system --realtime --disallow-exit --no-cpu-limit
+EOF
+sudo systemctl enable pulseaudio.service
